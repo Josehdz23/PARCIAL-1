11ffa67e298a77bcd5b43d9c17ba6d1d1fa61ce7
Josehdz23 <jose.jahr.2016@gmail.com>
2025-07-28 18:29:18 -0600

    Inicio del programa, se hace el menú y el main donde se llamaran a las funciones

--- Parcial 1.py
+++ Parcial 1.py
@@ -1,1 +1,29 @@
-print("Hola mundo")
+empleados = {}
+
+def menu():
+    print("\n= = = = MENÚ = = = =\n1. Registrar empleados. \n2. Mostrar empleados. \n3. Buscar empleados. \n4. Mostrar empleados con mejores evaluaciones. \n5. Empleado con mejor promedio\n6. Salir")
+
+def main():
+    while True:
+        try:
+            menu()
+            opcion = int(input("Seleccione una opción: "))
+            match opcion:
+                case 1:
+                    print()
+                case 2:
+                    print()
+                case 3:
+                    print()
+                case 4:
+                    print()
+                case 5:
+                    print()
+                case 6:
+                    print("SALIENDO . . .")
+                    break
+                case _:
+                    print("Esa opción no existe, reintente")
+        except Exception as ex:
+            print(f"Ha ocurrido un error: {ex}")
+menu()
